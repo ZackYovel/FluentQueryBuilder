621c234ac1bd9ffdb7154dfb0ea42eae15c0376f
Yehezkel (Zack) Yovel <yovel.zack@gmail.com>
2014-07-01 08:58:28 +0300

    Update SelectBuilder.php

--- Zack/FluentDBAL/SelectBuilder.php
+++ Zack/FluentDBAL/SelectBuilder.php
@@ -17,7 +17,7 @@
  * limitations under the License.
  */
 
-class SelectBuilder {
+class SelectBuilder extends AbstractQuery {
 
 //    private $crud;
     private $selection;
@@ -103,17 +103,4 @@ class SelectBuilder {
         }
         return $result;
     }
-
-    private function getConnection($host, $dbname, $user, $password, $encoding = "'utf8'") {
-        try {
-            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $pdo->exec("SET NAMES $encoding");
-            return $pdo;
-        } catch (PDOException $e) {
-            print "Error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-    }
-
 }
