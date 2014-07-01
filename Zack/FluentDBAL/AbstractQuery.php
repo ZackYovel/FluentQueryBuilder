9b5f3995991657326d70b6894a7e1134af8209b4
Yehezkel (Zack) Yovel <yovel.zack@gmail.com>
2014-07-01 08:58:48 +0300

    Update AbstractQuery.php

--- Zack/FluentDBAL/AbstractQuery.php
+++ Zack/FluentDBAL/AbstractQuery.php
@@ -1,4 +1,15 @@
 <?php
 
 abstract class AbstractQuery{
+      private function getConnection($host, $dbname, $user, $password, $encoding = "'utf8'") {
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec("SET NAMES $encoding");
+            return $pdo;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
 }
